daa975ee716c10db70bde1ae9edc9840277790a5
josecortorreal <josebibby23@gmail.com>
2023-04-29 21:08:24 -0400

    created function to READme file , and function to intialize app

--- index.js
+++ index.js
@@ -55,10 +55,24 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README file generated!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+      const markdown = generateMarkdown(data);
+      writeToFile('README.md', markdown);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 // Function call to initialize app
 init();
